21a4271f8ee7af8abec30d02ac1c41fe5f530705
mrgenius17 <darshanthakkar716@gmail.com>
2023-01-20 21:12:21 +0530

    Solve 2nd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, repititions) {
+    if(repititions<0) return "ERROR";
+    let res = "";
+    while(repititions>0){
+        res += str;
+        repititions--;
+    }
+    return res;
 };
 
 // Do not edit below this line
