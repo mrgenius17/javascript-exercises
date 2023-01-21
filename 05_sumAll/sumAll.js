9be05c592d6b817dbb4469412aa09770ca0862e5
mrgenius17 <darshanthakkar716@gmail.com>
2023-01-21 13:08:50 +0530

    Solve 05 successfully

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if(typeof start != "number" || typeof end != "number") return "ERROR";
+    if(start<0 || end<0) return "ERROR";
+    if(start>end){
+        let mid = start;
+        start=end;
+        end = mid;
+    }
+    let sum = 0;
+    while(start<=end){
+        sum += start;
+        start++;
+    }
+    return sum;
 };
 
 // Do not edit below this line
