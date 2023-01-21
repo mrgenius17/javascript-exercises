f0fd51c9b47e4b2d3981a67cfc27c4be9e091fcc
mrgenius17 <darshanthakkar716@gmail.com>
2023-01-21 12:39:21 +0530

    Solve 03 successfully

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const letters = [];
+    for(let i=0; i<str.length; i++){
+        letters.push(str.charAt(i));
+    }
+    let reverse = "";
+    for(let i=letters.length - 1; i>=0; i--){
+        reverse += letters[i];
+    }
+    return reverse;
 };
 
 // Do not edit below this line
