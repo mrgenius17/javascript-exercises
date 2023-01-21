49131f65b92866d440960ffe7dff7607ef7f94ba
mrgenius17 <darshanthakkar716@gmail.com>
2023-01-21 13:01:21 +0530

    Solve 04 successfully

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for(const item of args){
+        let index = array.indexOf(item);
+        if(index!=-1) array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
